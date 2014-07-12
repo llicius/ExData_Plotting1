4b146d6248c1d0a152d59244e479bbd74bf32010
llicius <fabricio.in.japan@gmail.com>
2014-07-12 17:21:54 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,10 +1,13 @@
-source("prep.R")
-
-doPlot1 <- function() {
-    tbl <- prepareData()
-    png(filename = "plot1.png", width = 480, height = 480, units = "px")
-    hist(tbl$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
-    dev.off()
-}
-
-doPlot1()
+power = read.csv("household_power_consumption.txt", header=T,
+    sep=";", stringsAsFactors=F, na.strings="?",
+    colClasses=c("character", "character", "numeric",
+    "numeric", "numeric", "numeric",
+    "numeric", "numeric", "numeric"))
+power$Date = as.Date(power$Date, format="%d/%m/%Y")
+startDate = as.Date("01/02/2007", format="%d/%m/%Y")
+endDate = as.Date("02/02/2007", format="%d/%m/%Y")
+power = power[power$Date >= startDate & power$Date <= endDate, ]
+png(filename="plot1.png", width=480, height=480)
+hist(df.power$Global_active_power, main="Global Active Power",
+xlab="Global Active Power (kilowatts)", col="red")
+dev.off()
