a96a7acca1296f303d13e99da280d3add5dcdb85
llicius <fabricio.in.japan@gmail.com>
2014-07-13 14:56:05 +0200

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,25 @@
+power = read.csv("household_power_consumption.txt", header=T,
+    sep=";", stringsAsFactors=F, na.strings="?",
+    colClasses=c("character", "character", "numeric",
+     "numeric", "numeric", "numeric",
+     "numeric", "numeric", "numeric"))
+power$timestamp = strptime(paste(power$Date, power$Time),
+  format="%d/%m/%Y %H:%M:%S", tz="UTC")
+startDate = strptime("01/02/2007 00:00:00", format="%d/%m/%Y %H:%M:%S", tz="UTC")
+endDate = strptime("02/02/2007 23:59:59", format="%d/%m/%Y %H:%M:%S", tz="UTC")
+power = power[power$timestamp >= startDate & power$timestamp <= endDate, ]
+png(filename="plot4.png", width=480, height=480)
+par(mfcol=c(2,2))
+plot(power$timestamp, power$Global_active_power, type="l", xlab="",
+     ylab="Global Active Power")
+plot(power$timestamp, power$Sub_metering_1, type="l", xlab="",
+     ylab="Energy sub metering")
+lines(power$timestamp, power$Sub_metering_2, col="red")
+lines(power$timestamp, power$Sub_metering_3, col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("black", "red", "blue"), lwd=par("lwd"), bty="n")
+plot(power$timestamp, power$Voltage, type="l",
+     xlab="datetime", ylab="Voltage")
+plot(power$timestamp, power$Global_reactive_power, type="l",
+     xlab="datetime", ylab="Global_reactive_power")
+dev.off()
